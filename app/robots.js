b0d81af82069c5f95e37d57bf28d34a2f2988c0f
Chrisfaherty <chrismactom@gmail.com>
2026-03-17 15:30:39 +0000

    SEO: robots.txt with crawl rules

--- app/robots.js
+++ app/robots.js
@@ -1,6 +1,7 @@
 export default function robots() {
   return {
-    rules: { userAgent: '*', allow: '/', disallow: '/api/' },
+    rules: [{ userAgent: '*', allow: '/', disallow: ['/api/', '/admin'] }],
     sitemap: 'https://www.aranponytrap.com/sitemap.xml',
+    host: 'https://www.aranponytrap.com',
   }
 }
